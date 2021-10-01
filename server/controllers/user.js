454a9707be066589f5195cf2c8ebba072d1911bb
raculber <rculbert5678@gmail.com>
2021-10-01 00:08:21 -0500

    fixing sign up authentication

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,27 +3,39 @@ import express from "express";
 import User from "../models/user.js";
 
 import bcrypt from "bcrypt";
+import emailValidator from "email-validator";
 
 const router = express.Router();
 
 export const createUser = async (req, res) => {
   console.log(req.body);
-  const { enteredUser, enteredEmail, enteredPassword, reenteredPassword } =
+  const { enteredUser, enteredEmail, enteredPassword, reenteredPass } =
     req.body;
-  const findUser = User.findOne({ enteredUser });
-  const findEmail = User.findOne({ enteredEmail });
 
-  if (findUser || findEmail) return res.send("Email already exists");
-  else if (enteredPassword !== reenteredPassword)
-    return res.send("Passwords must match");
+  enteredUser = enteredUser.trim();
+  enteredEmail = enteredEmail.trim();
+  enteredPassword = enteredPassword.trim();
+  reenteredPass = reenteredPass.trim();
+
+  const findUser = User.findOne({ username: enteredUser });
+  const findEmail = User.findOne({ email: enteredEmail });
+
+  if (findUser.username || findEmail.enteredEmail)
+    return res.json({ message: "User already exists" });
+  else if (enteredPassword !== reenteredPass)
+    return res.json({ message: "Passwords must match" });
+  else if (!emailValidator.validate(enteredEmail))
+    return res.json({ message: "Invalid email" });
+  else if (enteredPassword.length < 6)
+    return res.json({ message: "Password must be at least six characters" });
   else {
     try {
       const securedPass = await bcrypt.hash(enteredPassword, 12);
       const User = new User({ enteredUser, enteredEmail, securedPass });
       User.save();
-      res.send("Success");
+      res.json({ message: "Success" });
     } catch (error) {
-      res.send(error);
+      res.json({ message: error });
     }
   }
 };
--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,20 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: String,
-  email: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
