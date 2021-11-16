47a6a4b3068a0d78f36db2d7cd3b70bcf6b58f55
kaylaboyd430 <70405463+kaylaboyd430@users.noreply.github.com>
2021-11-15 22:34:00 -0600

    Create SignUp template and update READ.ME

--- client/src/components/Auth/SignUp.js
+++ client/src/components/Auth/SignUp.js
@@ -1,78 +1,137 @@
-import { useRef, useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router";
-import { addUser } from "../../store/auth-slice";
-import axios from "axios";
-import "../../styles/SignIn.css"
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const SignUp = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const emailRef = useRef("");
-  const passRef = useRef("");
-  const reenteredPassRef = useRef("");
-  const [token, setToken] = useState("");
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-  useEffect(() => {
-    localStorage.setItem("token", token);
-    if (token !== "") {
-      axios
-        .get("http://localhost:3001/api/auth", {
-          headers: {
-            "access-token": localStorage.getItem("token"),
-          },
-        })
-        .then((res) => {
-          console.log(res);
-          history.push("/");
-        })
-        //Use this code block if user not authenticated
-        .catch((err) => {
-          console.log(err);
-          
-        });
-    }
-  }, [token, history]);
+const theme = createTheme();
 
-  const validateSignUp = (event) => {
+export default function SignUp() {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const userInfo = {
-      enteredEmail: emailRef.current.value,
-      enteredPassword: passRef.current.value,
-      reenteredPassword: reenteredPassRef.current.value,
-    };
-    axios
-      .post("http://localhost:3001/api/sign-up", userInfo)
-      .then((res) => {
-        // res.data.message will contain necessary info about why
-        // sign up/in failed
-        console.log(res);
-        if (res.data.message) {
-          // Handler server error in this code block
-        } else if (res.data.token) {
-          dispatch(
-            addUser({
-              userId: res.data.result.userId,
-              email: res.data.result.enteredEmail,
-            })
-          );
-          setToken(res.data.token);
-        }
-      })
-      .catch((err) => console.log(err));
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
   };
 
   return (
-    <form onSubmit={validateSignUp}>
-      <input ref={emailRef} placeholder="Email" type="text" id="email"></input>
-      <br></br><br></br>
-      <input ref={passRef} placeholder="Password" type="password" id="password"></input>
-      <br></br><br></br>
-      <input ref={reenteredPassRef} placeholder="Confirm Password" type="password" id="passwordretype"></input>
-      <br></br><br></br>
-      <button type="submit">Create Account</button>
-    </form>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="retypePassword"
+                  label="Retype Password" //look at difference btw label, type...
+                  type="retypePassword"
+                  id="retypePassword"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="http://localhost:3000/sign-in" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default SignUp;
+}
