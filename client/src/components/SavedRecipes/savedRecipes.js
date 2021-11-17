0e536cca89339b6d5f37847d4fa58278b150ac04
kaylaboyd430 <70405463+kaylaboyd430@users.noreply.github.com>
2021-11-16 20:08:38 -0600

    fix error in savedRecipes.js

--- client/src/components/SavedRecipes/savedRecipes.js
+++ client/src/components/SavedRecipes/savedRecipes.js
@@ -14,52 +14,52 @@ import { useEffect, useState } from "react";
 
 
 
-// const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-// const theme = createTheme();
+const theme = createTheme();
 
-// export default function savedRecipes() {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-//       <AppBar position="relative">
-//         <Toolbar>
-//         </Toolbar>
-//       </AppBar>
-//       <main>
-//         {/* Hero unit */}
-//         <Box
-//           sx={{
-//             bgcolor: 'background.paper',
-//             pt: 8,
-//             pb: 6,
-//           }}
-//         >
-//           <Container maxWidth="sm">
-//             <Typography
-//               component="h1"
-//               variant="h2"
-//               align="center"
-//               color="text.primary"
-//               gutterBottom
-//             >
-//               Pantry
-//             </Typography>
-//           </Container>
-//         </Box>
-//         <Container sx={{ py: 8 }} maxWidth="md">
-//           {/* End hero unit */}
-//           <Grid container spacing={4}>
-//             {cards.map((card) => (
-//               <Grid item key={card} xs={12} sm={6} md={4}>
+export default function savedRecipes() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Pantry
+            </Typography>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
 
     
 
-//               </Grid>
-//             ))}
-//           </Grid>
-//         </Container>
-//       </main>
-//     </ThemeProvider>
-//   );
-// }
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+  );
+}
